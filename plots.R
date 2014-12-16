4567afd1079ec9560f818dbbc4c271260155176f
lmendoza <leemendoza@gmail.com>
2014-12-16 06:52:53 -0600

    updated plots file

--- plots.R
+++ plots.R
@@ -4,7 +4,24 @@ library(ggplot2)
 ## set working directory
 setwd(".\\Github\\EDA-Project2")
 
-## assumes that the data files are in a subfolder named 'data'
+## assumes that the source data files are in a subfolder named 'data'
+
+## NEI will contain all of the PM2.5 emissions data for 1999, 2002, 2005, and 2008. 
+## For each year, the table contains number of tons of PM2.5 emitted from a specific 
+## type of source for the entire year. 
 NEI <- readRDS("data\\summarySCC_PM25.rds")
 SCC <- readRDS("data\\Source_Classification_Code.rds")
-boxplot(Emissions~year, data=NEI, ylab="Total Emmissions", xlab = "Year", ylim = c(0.0, 25000))
+
+
+## Plot 1
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+## Using the base plotting system, make a plot showing the total PM2.5 emission from 
+## all sources for each of the years 1999, 2002, 2005, and 2008.
+## Get aggregate totals for each year
+totalEmissions = aggregate(Emissions ~ year, data = NEI, sum)
+## Show 'em
+barplot(totalEmissions$Emissions, 
+        names.arg = totalEmissions$year, 
+        xlab = "Year", 
+        ylab = expression('PM'[25]*' Emissions (Tons)') )
+
